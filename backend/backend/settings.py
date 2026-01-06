9337267d90436b3824d4e4c9916b2f2cfbdaa775
vodoleikin <vodoleikin@yandex.ru>
2026-01-06 15:29:21 +0300

    06.01.2025. 15.30. Some corrects in backend/backend/settings.py, added docker-compose.production.yml.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'like-taski.ddns.net', '158.160.91.191']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000', 'like-taski.ddns.net', '158.160.91.191']
 
 
 # Application definition
